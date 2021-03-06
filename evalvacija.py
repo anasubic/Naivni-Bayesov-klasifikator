8439841e0a2f2e2d86e946985c03a43d2e69a62d
anasubic <subic.ana2@gmail.com>
2021-03-06 23:38:47 +0100

    Cross-validation na podatkovnem naboru Iris Data Set

--- evalvacija.py
+++ evalvacija.py
@@ -7,25 +7,36 @@ podatki = arff.loadarff('dataset_61_iris.arff')[0]
 podatki = pd.DataFrame(podatki)
 podatki = np.asarray(podatki)
 
-slovar = {b'Iris-virginica':0, b'Iris-setosa':1, b'Iris-versicolor':2}
+slovar = {b'Iris-virginica': 0, b'Iris-setosa': 1, b'Iris-versicolor': 2}
+st_razredov = 3
 podatki = podatki.T
-prevedeno = np.copy(podatki[-1])
-for k, v in slovar.items(): prevedeno[podatki[-1]==k] = v
-podatki[-1] = prevedeno
+razredi = []
+for podatek in podatki[-1]:
+    razredi.append(slovar[podatek])
+podatki[-1] = np.asarray(razredi)
 podatki = podatki.T
 
-delitev = 0.3
-dolzina_podatkov = len(podatki)
-np.random.shuffle(podatki)
-test, train = np.split(podatki, [int(dolzina_podatkov*delitev)])
+# np.random.shuffle(podatki)
 
-povp_razred, std_razred, velikost_razredov = kf.statistika(kf.sortiraj_po_razredih(train, 3))
-st_razredov = len(velikost_razredov)
+bloki = np.array_split(podatki, 10)
+rezultati_evalvacij = []
 
-p, d = 0, len(test)
-for podatek in test:
-	verjetnosti = kf.razvrsti(podatek,st_razredov,povp_razred,std_razred,velikost_razredov)
-	if podatek[-1] == verjetnosti.index(max(verjetnosti)):
-		p += 1
+# naredimo 10 blokov, trening in razvrstitev testnega vzorca
+for a in range(10):
+	test = bloki[a]
+	trening = np.copy(bloki)
+	trening = np.delete(trening, a, axis=0)
+	trening = np.concatenate(trening, axis=0)
+	razredi = kf.sortiraj_po_razredih(trening, st_razredov)
+	povp_razred, std_razred, velikost_razredov = kf.statistika(razredi)
 
-print("Pravilno: " + str(round((p/d)*100,2)) + "%")
+	d, p = len(test), 0
+	for podatek in test:
+		verjetnosti = kf.razvrsti(podatek, st_razredov, povp_razred, std_razred,
+								            velikost_razredov)
+		if podatek[-1] == verjetnosti.index(max(verjetnosti)):
+			p += 1
+	rezultati_evalvacij.append(p/d)
+zanesljivost = np.sum(rezultati_evalvacij)*10
+
+print(zanesljivost)
--- klasifikator.py
+++ klasifikator.py
@@ -1,49 +1,35 @@
 import numpy as np
 
-dataset = [[3.393533211,2.331273381,0],
- [3.110073483,1.781539638,0],
- [1.343808831,3.368360954,0],
- [3.582294042,4.67917911,0],
- [2.280362439,2.866990263,0],
- [7.423436942,4.696522875,1],
- [5.745051997,3.533989803,1],
- [9.172168622,2.511101045,1],
- [7.792783481,3.424088941,1],
- [7.939820817,0.791637231,1]]
 
-# razredi od 0 do N, če so drugačni se jih prej prevede s slovarjem
 def sortiraj_po_razredih(podatki, st_razredov):
- razredi = [[] for razred in range(st_razredov)]
- for podatek in podatki:
-  razredi[podatek[-1]].append(podatek)
- return razredi
+    razredi = [[] for razred in range(st_razredov)]
+    for podatek in podatki:
+        razredi[podatek[-1]].append(podatek)
+    return razredi
 
 def statistika(razredi):
- povp_razred = []
- std_razred = []
- velikost_razredov = []
- for razred in razredi:
-  razred = np.array(np.stack(razred,axis=0), dtype=np.float64)
-  povp_razred.append(np.average(razred, axis=0))
-  std_razred.append(np.std(razred, axis=0, ddof=1))
-  velikost_razredov.append(len(razred))
- #statisticni_podatki = np.asarray([np.asarray(povp_razred), np.asarray(std_razred), np.asarray(velikost_razredov)])
- return np.asarray(povp_razred), np.asarray(std_razred), np.asarray(velikost_razredov)
+	povp_razred = []
+	std_razred = []
+	velikost_razredov = []
+	for razred in razredi:
+		razred = np.array(np.stack(razred, axis=0), dtype=np.float64)
+		povp_razred.append(np.average(razred, axis=0))
+		std_razred.append(np.std(razred, axis=0, ddof=1))
+		velikost_razredov.append(len(razred))
+	return np.asarray(povp_razred), np.asarray(std_razred), np.asarray(velikost_razredov)
 
 def gaussova_porazdelitev(x, povp, std):
- gauss = (1/(std * np.sqrt(2*np.pi)))*np.exp(-1/2*((x - povp)/std)**2)
- return gauss
+	gauss = (1/(std * np.sqrt(2*np.pi)))*np.exp(-1/2*((x - povp)/std)**2)
+	return gauss
 
 def razvrsti(podatek_test, st_razredov, povp_razred, std_razred, velikost_razredov):
- velikost_podatkov = np.sum(velikost_razredov)
- vse_verjetnosti = []
- for a in range(st_razredov):
-  pojavnost_razreda = velikost_razredov[a] / velikost_podatkov
-  verjetnost = pojavnost_razreda
-  for b in range(len(podatek_test)-1):
-   verjetnost = verjetnost * gaussova_porazdelitev(podatek_test[b], povp_razred[a][b], std_razred[a][b])
-  vse_verjetnosti.append(verjetnost)
- return vse_verjetnosti
-
-
-
+	velikost_podatkov = np.sum(velikost_razredov)
+	vse_verjetnosti = []
+	for a in range(st_razredov):
+		pojavnost_razreda = velikost_razredov[a] / velikost_podatkov
+		verjetnost = pojavnost_razreda
+		for b in range(len(podatek_test)-1):
+			verjetnost = verjetnost * gaussova_porazdelitev(podatek_test[b],
+															povp_razred[a][b], std_razred[a][b])
+		vse_verjetnosti.append(verjetnost)
+	return vse_verjetnosti
